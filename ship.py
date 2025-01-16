15ece32aa62a8a28cc98a50726a046e2ad674c38
Ben Power <ben.power8613@gmail.com>
2025-01-15 20:46:12 -0400

    added collisions and game-ending logic

--- ship.py
+++ ship.py
@@ -40,3 +40,8 @@ class Ship:
         "Draw the ship at its current location"
         self.screen.blit(self.image, self.rect)
 
+    
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
